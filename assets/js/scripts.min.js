b0b6d376680e81c7d64eeaa42f9d5e5a37bb993f
Slava Tyhonchuk <slavatyhonchuk@gmail.com>
2018-07-10 17:06:16 +0300

    modal

--- assets/js/scripts.min.js
+++ assets/js/scripts.min.js
@@ -8,12 +8,57 @@
         preloader: $('.preloader'),
         modalWindow: $('.modal'),
 
-        init: function () {
-
+        init() {
+            btcPay.initModal();
         },
+        initModal() {
 
+            let openModal = $('.openModal'),
+                modal = $('.modal-container');
+
+            //  calls
+            openClicks();
+            // 
+
+            function openClicks() {
+                openModal.each(function () {
+                    $(this).on('click', function (e) {
+                        e.preventDefault();
+                        let dataOpenModal = $(this).attr('data-open-modal');
+                        openModalWindow(dataOpenModal);
+                    });
+                });
+            }
+
+            function openModalWindow(dataOpenModal) {
+                modal.each(function () {
+                    let modal = $(this).attr('data-modal');
+
+                    if (modal == dataOpenModal) {
+                        $(this).addClass('modal-visible');
+                    }
+                }, closeModal()); // callback
+            }
+
+            function closeModal() {
+                modal.each(function () {
+                    $(this).on('click', function (e) {
+                        if (e.target !== e.currentTarget) return false;
+                        $(this).removeClass('modal-visible');
+                    });
+                    $(document).keyup(function (e) {
+                        if (e.keyCode == 27) {
+                            modal.removeClass('modal-visible');
+                        }
+                    });
+                });
+
+            }
+        }
     });
 
-    btcPay.init();
+    $(document).ready(function () {
+        btcPay.init();
+    });
 
 })(jQuery);
